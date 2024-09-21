a72b7f439eaab11d08ed22bc6612969062c78654
adanovanurzada <adanovanurzada0@gmail.com>
2024-09-21 12:11:11 +0600

    nurzada commit

---
+++ lesson_11.py
@@ -0,0 +1,26 @@
+'''file = open('file.txt', 'w')
+file.write('Бишкек, Кыргызстан')
+file.close()'''
+
+'''an:class Hum
+    #класс - (шаблон нашего обьекта)
+    brain = True
+    hearth = True
+    #атрибути на уровне класса
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        #конструктор класса
+
+    def hello_people(self):
+        return f'{self.name} привет, тебе {self.age} лет'
+
+
+Nurzada = Human(name:'Nurzada', age:16)
+
+print(Human.hello_people(Nurzada))'''
+
+
+
+
---
+++ python.py
@@ -0,0 +1,887 @@
+'''san = int(input('Введите трехзначные цифры:'))
+if san >= 100 and san <= 999:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+
+'''слова = str(input('Введите палиндром слова ').lower()
+if слова == слова[::-1]:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+'''c = 0
+while True:
+    number = int(input('Введите число(пока он не введет отрицательное: '))
+    print('kolechstvo',c')
+    if number < 0:
+        break
+c += 1 '''
+
+
+'''words[2], words[5] = words[5], words[2]
+print(words)
+
+words[3] =('python')
+print(words)
+
+del words[4]
+print(words)
+
+words.insert(1,34)
+print(words)
+words = [True, False, 12, -56, 'python', 'java', 3.45]'''
+
+
+'''words = ['h', б.13, 'C', 'e', 'T', True, 'k', 'e', 3, 'e', 1, 'g']
+del words[1]
+print(words)
+
+words.insert(12,'letters')
+words.insert(12,numbers)
+
+print(words)
+
+words.append(True)
+print(words)
+
+words.reverse()'''
+
+
+'''objects = (True, 6.13, 'python', 12)
+
+objects = list(objects)
+objects.remove('python')
+print(objects)
+objects.append('java')
+print(objects)
+objects[0], objects[-1] = objects[-1], objects[0]
+print(objects)'''
+
+
+'''san = int(input('Введите трехзначные цифры:'))
+if san >= 100 and san <= 999:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+
+'''слова = str(input('Введите палиндром слова ').lower()
+if слова == слова[::-1]:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+'''c = 0
+while True:
+    number = int(input('Введите число(пока он не введет отрицательное: '))
+    print('kolechstvo',c')
+    if number < 0:
+        break
+c += 1 '''
+
+
+'''words[2], words[5] = words[5], words[2]
+print(words)
+
+words[3] =('python')
+print(words)
+
+del words[4]
+print(words)
+
+words.insert(1,34)
+print(words)
+words = [True, False, 12, -56, 'python', 'java', 3.45]'''
+
+
+'''words = ['h', б.13, 'C', 'e', 'T', True, 'k', 'e', 3, 'e', 1, 'g']
+del words[1]
+print(words)
+
+words.insert(12,'letters')
+words.insert(12,numbers)
+
+print(words)
+
+words.append(True)
+print(words)
+
+words.reverse()'''
+
+
+'''objects = (True, 6.13, 'python', 12)
+
+objects = list(objects)
+objects.remove('python')
+print(objects)
+objects.append('java')
+print(objects)
+objects[0], objects[-1] = objects[-1], objects[0]
+print(objects)'''
+
+
+
+
+
+
+
+
+
+
+'''san = int(input('Введите трехзначные цифры:'))
+if san >= 100 and san <= 999:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+
+'''слова = str(input('Введите палиндром слова ').lower()
+if слова == слова[::-1]:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+'''c = 0
+while True:
+    number = int(input('Введите число(пока он не введет отрицательное: '))
+    print('kolechstvo',c')
+    if number < 0:
+        break
+c += 1 '''
+
+
+'''words[2], words[5] = words[5], words[2]
+print(words)
+
+words[3] =('python')
+print(words)
+
+del words[4]
+print(words)
+
+words.insert(1,34)
+print(words)
+words = [True, False, 12, -56, 'python', 'java', 3.45]'''
+
+'''rds = ['h', б.13, 'C', 'e', 'T', True, 'k', 'e', 3, 'e', 1, 'g']
+del words[1]
+print(words)
+
+words.insert(12,'letters')
+words.insert(12,numbers)
+
+print(words)
+
+words.append(True)
+print(words)
+
+words.reverse()'''
+
+
+'''objects = (True, 6.13, 'python', 12)
+
+objects = list(objects)
+objects.remove('python')
+print(objects)
+objects.append('java')
+print(objects)
+objects[0], objects[-1] = objects[-1], objects[0]
+print(objects)'''
+
+
+
+
+'''san = int(input('Введите трехзначные цифры:'))
+if san >= 100 and san <= 999:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+
+'''слова = str(input('Введите палиндром слова ').lower()
+if слова == слова[::-1]:
+   print('ok')
+else:
+    print('no')'''
+
+
+
+'''c = 0
+while True:
+    number = int(input('Введите число(пока он не введет отрицательное: '))
+    print('kolechstvo',c')
+    if number < 0:
+        break
+c += 1 '''
+
+'''rds[2], words[5] = words[5], words[2]
+print(words)
+
+words[3] =('python')
+print(words)
+
+del words[4]
+print(words)
+
+words.insert(1,34)
+print(words)
+words = [True, False, 12, -56, 'python', 'java', 3.45]'''
+
+
+'''words = ['h', б.13, 'C', 'e', 'T', True, 'k', 'e', 3, 'e', 1, 'g']
+del words[1]
+print(words)
+
+words.insert(12,'letters')
+words.insert(12,numbers)
+
+print(words)
+
+words.append(True)
+print(words)
+
+words.reverse()'''
+
+
+'''objects = (True, 6.13, 'python', 12)
+
+objects = list(objects)
+objects.remove('python')
+print(objects)
+objects.append('java')
+print(objects)
+objects[0], objects[-1] = objects[-1], objects[0]
+print(objects)'''
+
+
+
+'''dents = {
+    'Айбек': [5, 4, 3],
+    'Бурул': [4, 5, 5],
+    'Гульназ': [3, 4, 4],
+    'Дастан': [5, 5, 4],
+    'Элиза': [4, 3, 5]
+}
+
+average_scores = {name: sum(grades) / len(grades) for name, grades in students.items()}
+
+excellent_students = [name for name, avg in average_scores.items() if avg == 5]
+
+good_students = [name for name, avg in average_scores.items() if 4 <= avg < 5]
+
+print("Отличники:", excellent_students)
+print("Хорошисты:", good_students)'''
+
+
+
+
+
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+elements = list(set(a) & set(b))
+print(elements)'''
+
+
+
+#тип данных:int,float,bool,string,set,tuple,list,dict''''
+
+
+
+
+'''def int_to_roman(number):
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    ]
+   рим  = [
+        'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'
+    ]
+   сан = [
+
+  roman_number = ''
+  i = 0
+  while number > 0:
+     for _ in range(number // сан[i]):
+         roman_number += рим[i]
+         number -= сан[i]
+     i += 1
+ return roman_number
+
+    number = 1994
+    roman_number = int_to_roman(number)
+    print(roman_number)'''
+
+
+'''def reverse_words_in_sentence(sentence):
+
+    words = sentence.split()
+
+    reversed_words = [word[::-1] for word in words]
+
+    reversed_sentence = " ".join(reversed_words)
+
+    return reversed_sentence
+
+
+sentence = "Python - это круто!"
+result = reverse_words_in_sentence(sentence)
+print(result) '''
+
+
+
+
+
+
+
+
+'''def students(name, age):
+    print(f'{name}, привет тебе {age} лет')
+    if age >= 18:
+else:
+    pr
+name = input('Введите имя ')
+age = int(input('Введите возраст'''
+
+
+
+
+'''contact = [
+    {'name':'Geeks','phone':'0507052018'},
+    {'name':'Служба спасения','phone':'911'},
+    {'name':'Пожарная служба','phone':'110'},
+]
+def create_contact(name, phone):
+    for contact in contacts:
+        if contact['name'] == name or contact['phone'] == phone:
+            print("Контакт с таким именем или номером уже существует.")
+            return
+    contacts.append({'name': name, 'phone': phone})
+    print(f"Контакт {name} добавлен.")
+def edit_contact(name):
+    for contact in contacts:
+        if contact['name'] == name:
+            new_name = input("Введите новое имя: ")
+            new_phone = input("Введите новый номер: ")
+            for c in contacts:
+                if c['name'] == new_name or c['phone'] == new_phone:
+                    print("Контакт с таким именем или номером уже существует.")
+                    return
+
+            contact['name'] = new_name
+            contact['phone'] = new_phone
+            print(f"Контакт {name} изменен на {new_name}.")
+            ret
+
+
+
+def delete_contact(name):
+    for contact in contacts:
+        if contact['name'] == name:
+            contacts.remove(contact)
+            print(f"Контакт {name} удалён.")
+            return
+    print("Контакт не найден.")
+
+
+def main():
+    while True:
+        print("\nТекущие контакты:")
+        for contact in contacts:
+            print(f"{contact['name']}: {contact['phone']}")
+
+        print("Выберите действие:")
+        print("1. Контакт кошуу")
+        print("2. Контакт озгортуу")
+        print("3. Контакт удалить")
+        print("4. Чыгуу")
+
+        choice = input("Ваш выбор: ")
+
+        if choice == '1':
+            name = input("Введите имя нового контакта: ")
+            phone = input("Введите номер нового контакта: ")
+            create_contact(name, phone)
+        elif choice == '2':
+            name = input("Введите имя контакта для изменения: ")
+            edit_contact(name)
+        elif choice == '3':
+            name = input("Введите имя контакта для удаления: ")
+            delete_contact(name)
+        elif choice == '4':
+            print("Программа завершена.")
+            break
+        else:
+            print("Неверный выбор, попробуйте снова.")
+
+
+if name == "main":
+    main()'''
+
+
+
+
+
+
+
+
+
+'''from random import randint
+
+random_number = randint(1, 100)
+c = 0
+
+while True:
+    c += 1
+    if c <= 6:
+        user = int(input("введите число "))
+        if user > random_number:
+            print(f"загаданное число меньше чем {user}")
+        elif user < random_number:
+            print(f"загаданное число больше чем {user}")
+        elif user == random_number:
+            print(f"ты выиграл это было число {random_number}")
+            break
+    else:
+        print(f"ты проиграл у тебя было число {random_number}")
+        break'''
+
+
+
+
+'''from random import randint
+
+random_number = randint(1, 100)
+c = 0
+points = 0
+
+
+while True:
+    c+=1
+    if c <= 10:
+
+        user = int(input("Введите число: "))
+
+        if user in guessed_numbers:
+            print("Вы уже вводили это число. Попробуйте другое.")
+
+        guessed_numbers.append(user)
+
+        if user > random_number:
+            print(f"Загаданное число меньше чем {user}")
+            points -= 1
+        elif user < random_number:
+            print(f"Загаданное число больше чем {user}")
+            points -= 1
+        elif user == random_number:
+            points += 10
+            print(f"Ты выиграл! Это было число {random_number}.")
+            print(f"Твои очки: {points}")
+            break
+
+        give_hint(user, random_number)
+    else:
+        print(f"Ты проиграл. Загаданное число было {random_number}.")
+        print(f"Твои очки: {points}")
+        break'''
+
+
+
+'''file = open('file.txt', 'w')
+file.write('Бишкек, Кыргызстан')
+file.close()'''
+
+
+
+
+
+
+
+
+
+
+'''class Human:    # класс - (шаблон нашего объекта)
+
+    brain = True     # аргументы на уровне класса
+    hearth = True
+
+    def __init__(self, name, age):    # конструктор класса
+        self.name = name
+        self.age = ag'''
+
+
+
+'''class Human:  # класс - (шаблон нашего объекта)
+
+    brain = True  # аргументы на уровне класса
+    hearth = True
+
+    def init(self, name, age):  # конструктор класса
+        self.name = name
+        self.age = age
+
+    def hello_people(self):  # метод
+        if self.age >= 18:
+            return f"{self.name} привет, тебе {self.age} лет. Ты совершеннолетний."
+        else:
+            return f"{self.name} привет, тебе {self.age} лет. Ты не совершеннолетний."
+
+# объекты
+nurzada = Human("Nurzada", 16)
+azamgul = Human("Azamgul", 16)
+
+print(Human.hello_people(nurzada))
+print(Human.hello_people(azamgul))
+print(Human.chekc)'''
+
+
+'''class Human:    # класс - (шаблон нашего объекта)
+
+    brain = True     # аргументы на уровне класса
+    hearth = True
+
+    def __init__(self, name, age):    # конструктор класса
+        self.name = name
+        self.age = age
+
+    def hello_people(self):     # метод
+        return f"{self.name} привет, тебе {self.age} лет"
+
+    def check_age(self):
+        if self.age >= 18:
+            return f"{self.name} ты у нас совершенолетний"
+        else:
+            return f"{self.name} ты у нас не совершенолетний"
+
+
+
+bektur = Human("Bektur", 14)    # объект
+azamat = Human("Azamat", 18)
+
+
+print(Human.hello_people(bektur)) 
+print(Human.hello_people(azamat))
+print(Human.check_age(bektur))
+print(Human.check_age(azamat))'''
+
+
+'''class SuperHero:
+
+    people = 'people'
+
+
+    def init(self, name, nickname, superpower, health_points, catchphrase):
+        self.name = name
+        self.nickname = nickname
+        self.superpower = superpower
+        self.health_points = health_points
+        self.catchphrase = catchphrase
+
+
+    def print_name(self):
+        print(f"Hero's name: {self.name}")
+
+
+    def double_health(self):
+        self.health_points *= 2
+        print(f"Health doubled: {self.health_points}")
+
+
+    def str(self):
+        ego = "Confident"
+        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Ego: {ego}, Health: {self.health_points}"
+
+
+    def len(self):
+        return len(self.catchphrase)
+
+
+
+hero = SuperHero('Nurzada', 'Batman', 'Azemgul', 100, 'I am Batman!')
+
+
+hero.print_name()
+hero.double_health()
+print(hero)
+print(f"Catchphrase length: {len(hero)}")'''
+
+
+
+
+
+
+
+'''
+class SuperHero:
+    def __init__(self, name, hp, damage, fly=False):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        self.fly = fly
+
+    def multiply_hp(self):
+        self.hp *= 2
+        print(f"{self.name}'s Hp очки здоровья были удвоены: {self.hp}"
+              
+#воздушный герой             
+class AirHero(SuperHero):
+    hero_type = "Air"  
+
+    def multiply_hp(self):
+        self.hp = self.hp ** 4
+        self.fly = True
+        print(f"{self.name}'s HP очки здоровья теперь возведены в квадрат до: {self.hp}")
+
+    def fly_check(self):
+        if self.fly:
+            print(f"{self.name} Теперь летает! Летать в True_phrase.")
+
+# Земной герой
+class EarthHero(SuperHero):
+    hero_type = "Earth"  
+
+    def multiply_hp(self):
+        self.hp = self.hp ** 2
+        self.fly = True
+        print(f"{self.name}'s HP очки здоровья теперь возведены в квадрат до: {self.hp}")
+
+    def fly_check(self):
+        if self.fly:
+            print(f"{self.name} Теперь летает! Летать в True_phrase.")
+
+air_hero = AirHero(name="Batman", hp=100, damage=50)
+earth_hero = EarthHero(name="Rocky", hp=120, damage=60)
+
+air_hero.multiply_hp()
+air_hero.fly_check()
+
+earth_hero.multiply_hp()
+earth_hero.fly_check()
+
+# Класс Villain
+class Villain(SuperHero):
+    people = "Monster" 
+
+    def gen_x(self, hero):
+        hero.hp -= self.damage
+        print(f"{hero.name}'s HP очки здоровья были снижены на {self.damage} to: {hero.hp}")
+
+    def crit(self):
+     crit_damage = self.damage ** 2
+        print(f"Critical damage is: {crit_damage}")
+        return crit_damage
+    
+villain = Villain(name="Dark", hp=80, damage=40)
+
+villain.crit()
+
+villain.gen_x(earth_hero)'''
+
+
+
+
+
+# инкапсуляция
+
+# 1 - открытый, 2 - защищенный, 3 - скрытый
+
+'''class Cat:
+    def __init__(self, name, age, hunger=50, energy=50, mood="happy"):
+        self.name = name
+        self.age = age
+        self.hunger = hunger
+        self.energy = energy
+        self.mood = mood
+
+    def update_mood(self):
+        if self.hunger < 30:
+            self.mood = "hungry"
+        elif self.energy < 20:
+            self.mood = "tired"
+        else:
+            self.mood = "happy"
+
+    def eat(self):
+        self.hunger += 10
+        self.energy -= 5
+        self.update_mood()
+        print(f"{self.name}, поел теперь он сытый, \nНастроение: {self.mood}")
+
+    def sleep(self):
+        self.hunger -= 5
+        self.energy += 10
+        self.update_mood()
+        print(f"{self.name}, поспал и теперь полон энергии, \nНастроение: {self.mood}")
+
+    def play(self):
+        self.hunger -= 10
+        self.energy -= 10
+        self.update_mood()
+        print(f"{self.name}, поиграл и повеселился и устал, \nНастроение: {self.mood}")
+
+    def status(self):
+        print(f"Имя: {self.name}")
+        print(f"Возраст: {self.age}")
+        print(f"Сытость: {self.hunger}")
+        print(f"Энергия: {self.energy}")
+        print(f"Настроение: {self.mood}")
+
+
+my_cat = Cat("Том", 3)
+my_cat2 = Cat("Джерри", 3)
+
+
+while True:
+    print("Что сделать с котом? ")
+    print("1. Покормить ")
+    print("2. Поспать ")
+    print("3. Поиграть ")
+    print("4. Проверить статус ")
+    print("5. Выйти ")
+
+    choice = input("Выберите действие: ")
+
+    if choice == "1":
+        my_cat.eat()
+    elif choice == "2":
+        my_cat.sleep()
+    elif choice == "3":
+        my_cat.play()
+    elif choice == "4":
+        my_cat.status()
+    elif choice == "5":
+        print("Заходи еще поиграть мы тебя ждем!!!")
+        break
+    else:
+        print("Некорректный ввод, кошка обиделась на тебя ..'''
+
+
+
+'''class Bank:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def moneyX(self):
+        amount = input(f"{self.name} Эсебинизге канча акча кошосуз? ")
+        if amount>0:
+            self.__balance += amount
+            print(f"{self.__balance}сиздин балансыныз")
+
+    def kill(self):
+       self.__balance = 0
+       print(f"{self.__balance} Эсебинизде акча нолго барабар болду")
+
+
+    def jackot(self):
+        self.__balance *= 10
+        print(f"{self.__balance} Эсебиниздеги балансыз он ээсе кобойду")
+
+
+    def merge_balance(self,other):
+        if isinstance(other,Bank):
+            self.__balance += other.__balance
+            print(f"{self.__balance} Эсебиниз бирикти")
+
+    def get_balance(self):
+        return self.__balance
+    def trigger_jackot(self):
+        self.__jackot
+    
+client1 = Bank("nurzada",200)
+client2 = Bank("azemgul",200)
+
+(client1.moneyX())
+(client2.moneyX())
+(client1.kill())
+(client2.trigger_jackot())
+(client1.merge_balance())
+(client2.merge_balance())
+
+print(f"{client1.get_balance()} Нурзаданын балансы:")
+print(f"{client2.get_balance()} Аземгулдун балансы:")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''class InstallmentPlan:
+    def init(self, price):
+        self.price = price
+
+    def calculate_total_cost(self):
+        return self.
+
+class FixedInstallment (InstallmentPlan):
+    def init(self, price, month):
+        super().init(price)
+        self.month = month
+    def calculate_total_cost(self):
+        return self.price / self.month
+class VariableInstallment (InstallmentPlan):
+    def init(self, price, month, percent):
+        super().init(price)
+        self.month = month
+        self.procent= percent
+    def calculate_total_cost(self):
+        total_cost = self.price * (1 + self.procent / 200)
+        return total_cost / self.month
+
+    if name == "main":
+    fixed_installment = FixedInstallment(1300, 13)
+    print("Ежемесячная сумма фиксированной рассрочки:", fixed_installment.calculate_total_cost())
+
+    variable_installment = VariableInstallment(1300, 13, 3)
+    print("Ежемесячная сумма переменной рассрочки:", variable_installment.calculate_total_cost())'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
